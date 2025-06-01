5a1afc80a15553353b81ba75164001d61e2a19ae
Maikoll Soto <maikoll120@gmail.com>
2025-06-01 11:33:45 -0300

    Workshop submission

--- python-workshop/projects/python-workshop/smart_contracts/hello_world/contract.py
+++ python-workshop/projects/python-workshop/smart_contracts/hello_world/contract.py
@@ -1,8 +1,11 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, String, Box
 from algopy.arc4 import abimethod
 
-
 class HelloWorld(ARC4Contract):
+    def __init__(self) -> None:
+        self.github = Box(String, key=b"github")
+
     @abimethod()
     def hello(self, name: String) -> String:
+        self.github.value = String("maikoll120")
         return "Hello, " + name
